e22a2503d1bc07e554cd8da24fb83b8d311d1682
dezzw <dw@dezzw.com>
2025-07-29 13:16:56 -0400

    Apply all new changes

--- modules/init-completion.el
+++ modules/init-completion.el
@@ -39,168 +39,168 @@
     (yas-global-mode)
     (:option yas-verbosity 0)))
 
-;; (setup lsp-mode
-;;   (:defer (:require lsp-mode))
-;;   (:also-load lib-lsp)
-;;   (:option lsp-enable-folding nil
-;;            lsp-enable-text-document-color nil
-;;            lsp-enable-on-type-formatting nil
-;;            lsp-headerline-breadcrumb-enable nil
-;;            lsp-completion-provider :none
-;;            lsp-enable-snippet nil
-;;            lsp-semantic-tokens-enable t
-;;            lsp-enable-indentation nil
-;;            lsp-idle-delay 0.500
-;;            lsp-keymap-prefix "C-x L")
+(setup lsp-mode
+  (:defer (:require lsp-mode))
+  (:also-load lib-lsp)
+  (:option lsp-enable-folding nil
+           lsp-enable-text-document-color nil
+           lsp-enable-on-type-formatting nil
+           lsp-headerline-breadcrumb-enable nil
+           lsp-completion-provider :none
+           lsp-enable-snippet nil
+           lsp-semantic-tokens-enable t
+           lsp-enable-indentation nil
+           lsp-idle-delay 0.500
+           lsp-keymap-prefix "C-x L")
+  (:when-loaded
+    (:with-map lsp-mode-map
+      (:bind "M-<return>" lsp-execute-code-action))
+    (:hook lsp-lens-mode)
+    (:with-mode lsp-completion-mode
+      (:hook (lambda ()
+               (setf (alist-get 'lsp-capf completion-category-defaults)
+                     '((styles . (orderless flex)))))))
+    (:with-mode python-ts-mode
+      (:hook
+       lsp-inlay-hints-mode
+       (lambda ()
+         (require 'lsp-pyright)
+         (setq lsp-pyright-langserver-command "basedpyright")
+         (lsp-deferred))))
+
+    (:with-mode java-ts-mode
+      (:hook
+       (lambda ()
+         (require 'lsp-java)
+         (lsp-deferred))))
+
+    (:with-mode swift-mode
+      (:hook
+       (lambda ()
+         (require 'lsp-sourcekit)
+         (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp")
+         (lsp-deferred))))
+
+    (:with-mode lsp-tailwindcss-major-modes
+      (:hook
+       (lambda ()
+         (setq lsp-tailwindcss-add-on-mode t))))
+
+    (:with-mode (clojure-mode clojurescript-mode clojurec-mode)
+      (:hook
+       (lambda ()
+         (aggressive-indent-mode)
+         (setq-local completion-at-point-functions
+                     (list (cape-capf-super #'cider-complete-at-point #'lsp-completion-at-point)))
+         (lsp-deferred))))
+
+    (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)))
+
+(when (featurep 'lsp-mode)
+  (setup lsp-ui
+    (:load-after lsp-mode)
+    (:option lsp-ui-doc-max-height 8
+             lsp-ui-doc-max-width 72         ; 150 (default) is too wide
+             lsp-ui-doc-delay 0.75           ; 0.2 (default) is too naggy
+             ;; lsp-ui doc
+             lsp-ui-doc-show-with-mouse nil  ; don't disappear on mouseover
+             lsp-ui-doc-show-with-cursor t
+             ;; lsp-ui sideline
+             lsp-ui-sideline-show-hover nil
+             lsp-ui-sideline-show-code-actions nil
+             ;; lsp signature
+             lsp-signature-render-documentation nil)
+
+    (:with-map lsp-ui-mode-map
+      (:bind
+       [remap xref-find-definitions] lsp-ui-peek-find-definitions
+       [remap xref-find-references] lsp-ui-peek-find-references)))
+
+  (setup dap-mode
+    (:load-after lsp-mode)
+    (:when-loaded
+      (:option dap-auto-configure-features '(sessions locals controls tooltip))
+      (:with-map dap-mode-map
+        (:bind
+         "C-x D D" dap-debug
+         "C-x D d" dap-debug-last)))
+    (:with-mode python-ts-mode
+      (:hook
+       (lambda ()
+         (require 'dap-python)
+         (setq dap-python-debugger 'debugpy))))
+
+    (:with-mode java-ts-mode
+      (:hook
+       (lambda ()
+         (require 'dap-java))))
+
+    (:with-mode java-ts-mode
+      (:hook
+       (lambda ()
+         (require 'dap-java))))))
+
+;; (setup lsp-bridge
+;;   (:pkg (lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
+;;                     :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+;;                     :build (:not compile)))
+;;   (:option
+;;    lsp-bridge-python-command "python-for-lsp-bridge"
+
+;;    lsp-bridge-enable-completion-in-minibuffer t
+;;    lsp-bridge-enable-with-tramp t
+;;    ;; ui configuration
+;;    lsp-bridge-signature-show-function 'lsp-bridge-signature-show-with-frame
+;;    lsp-bridge-signature-show-with-frame-position "top-right"
+;;    lsp-bridge-enable-mode-line nil
+;;    lsp-bridge-enable-hover-diagnostic nil
+;;    lsp-bridge-enable-inlay-hint t
+;;    lsp-bridge-enable-org-babel t
+
+;;    ;; acm configuration
+;;    acm-candidate-match-function 'orderless-flex
+;;    acm-backend-yas-match-by-trigger-keyword t
+;;    acm-enable-capf t
+;;    acm-enable-tabnine t
+
+;;    ;;lsp-server configuartion
+;;    lsp-bridge-nix-lsp-server 'nixd
+;;    ;; (acm-enable-tempel t)
+;;    ;; (acm-enable-codeium t)
+;;    ;; (acm-enable-citre t)
+;;    ;; :bind (:map acm-mode-map
+;;    ;;         ("C-n" . acm-select-next)
+;;    ;;         ("C-p" . acm-select-prev))
+;;    )
 ;;   (:when-loaded
-;;     (:with-map lsp-mode-map
-;;       (:bind "M-<return>" lsp-execute-code-action))
-;;     (:hook lsp-lens-mode)
-;;     (:with-mode lsp-completion-mode
-;;       (:hook (lambda ()
-;;                (setf (alist-get 'lsp-capf completion-category-defaults)
-;;                      '((styles . (orderless flex)))))))
-;;     (:with-mode python-ts-mode
-;;       (:hook
-;;        lsp-inlay-hints-mode
-;;        (lambda ()
-;;          (require 'lsp-pyright)
-;;          (setq lsp-pyright-langserver-command "basedpyright")
-;;          (lsp-deferred))))
+;;     (add-to-list 'acm-backend-capf-mode-list 'clojure-mode)
+;;     (add-to-list 'acm-backend-capf-mode-list 'clojurescript-mode)
 
-;;     (:with-mode java-ts-mode
-;;       (:hook
-;;        (lambda ()
-;;          (require 'lsp-java)
-;;          (lsp-deferred))))
+;;     (setq lsp-bridge-get-project-path-by-filepath
+;;           (lambda (filepath)
+;;             (or (when-let* ((project (project-current nil (file-name-directory filepath)))
+;;                             (root (project-root project)))
+;;                   (expand-file-name root))
+;;                 (file-name-directory filepath))))
 
-;;     (:with-mode swift-mode
-;;       (:hook
-;;        (lambda ()
-;;          (require 'lsp-sourcekit)
-;;          (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp")
-;;          (lsp-deferred))))
+;;     (yas-global-mode 1)
+;;     (global-lsp-bridge-mode)))
 
-;;     (:with-mode lsp-tailwindcss-major-modes
-;;       (:hook
-;;        (lambda ()
-;;          (setq lsp-tailwindcss-add-on-mode t))))
+;; (setup flymake
+;;   (:defer (:require flymake))
+;;   (:when-loaded
+;;     ;; ??:? `flymake-no-changes-timeout` ???? nil ?,
+;;     ;; ???? `eglot-handle-notification` ? `:after` ???
+;;     (:option flymake-no-changes-timeout nil
+;;              ;; emacs@30 feature
+;;              flymake-show-diagnostics-at-end-of-line 'fancy)
+;;     ;; flymake-fringe-indicator-position 'right-fringe)
+;;     (:with-mode prog-mode (:hook flymake-mode))
+;;     (:with-mode emacs-lisp-mode (:hook (lambda()(flymake-mode -1))))))
 
-;;     (:with-mode (clojure-mode clojurescript-mode clojurec-mode)
-;;       (:hook
-;;        (lambda ()
-;;          (aggressive-indent-mode)
-;;          (setq-local completion-at-point-functions
-;;                      (list (cape-capf-super #'cider-complete-at-point #'lsp-completion-at-point)))
-;;          (lsp-deferred))))
-
-;;     (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)))
-
-;; (when (featurep 'lsp-mode)
-;;   (setup lsp-ui
-;;     (:load-after lsp-mode)
-;;     (:option lsp-ui-doc-max-height 8
-;;              lsp-ui-doc-max-width 72         ; 150 (default) is too wide
-;;              lsp-ui-doc-delay 0.75           ; 0.2 (default) is too naggy
-;;              ;; lsp-ui doc
-;;              lsp-ui-doc-show-with-mouse nil  ; don't disappear on mouseover
-;;              lsp-ui-doc-show-with-cursor t
-;;              ;; lsp-ui sideline
-;;              lsp-ui-sideline-show-hover nil
-;;              lsp-ui-sideline-show-code-actions nil
-;;              ;; lsp signature
-;;              lsp-signature-render-documentation nil)
-
-;;     (:with-map lsp-ui-mode-map
-;;       (:bind
-;;        [remap xref-find-definitions] lsp-ui-peek-find-definitions
-;;        [remap xref-find-references] lsp-ui-peek-find-references)))
-
-;;   (setup dap-mode
-;;     (:load-after lsp-mode)
-;;     (:when-loaded
-;;       (:option dap-auto-configure-features '(sessions locals controls tooltip))
-;;       (:with-map dap-mode-map
-;;         (:bind
-;;          "C-x D D" dap-debug
-;;          "C-x D d" dap-debug-last)))
-;;     (:with-mode python-ts-mode
-;;       (:hook
-;;        (lambda ()
-;;          (require 'dap-python)
-;;          (setq dap-python-debugger 'debugpy))))
-
-;;     (:with-mode java-ts-mode
-;;       (:hook
-;;        (lambda ()
-;;          (require 'dap-java))))))
-
-;;     (:with-mode java-ts-mode
-;;       (:hook
-;;        (lambda ()
-;;          (require 'dap-java))))))
-
-(setup lsp-bridge
-  (:pkg (lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
-                    :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
-                    :build (:not compile)))
-  (:option
-   lsp-bridge-python-command "python-for-lsp-bridge"
-
-   lsp-bridge-enable-completion-in-minibuffer t
-   lsp-bridge-enable-with-tramp t
-   ;; ui configuration
-   lsp-bridge-signature-show-function 'lsp-bridge-signature-show-with-frame
-   lsp-bridge-signature-show-with-frame-position "top-right"
-   lsp-bridge-enable-mode-line nil
-   lsp-bridge-enable-hover-diagnostic nil
-   lsp-bridge-enable-inlay-hint t
-   lsp-bridge-enable-org-babel t
-
-   ;; acm configuration
-   acm-candidate-match-function 'orderless-flex
-   acm-backend-yas-match-by-trigger-keyword t
-   acm-enable-capf t
-   acm-enable-tabnine t
-
-   ;;lsp-server configuartion
-   lsp-bridge-nix-lsp-server 'nixd
-   ;; (acm-enable-tempel t)
-   ;; (acm-enable-codeium t)
-   ;; (acm-enable-citre t)
-   ;; :bind (:map acm-mode-map
-   ;;         ("C-n" . acm-select-next)
-   ;;         ("C-p" . acm-select-prev))
-   )
-  (:when-loaded
-    (add-to-list 'acm-backend-capf-mode-list 'clojure-mode)
-    (add-to-list 'acm-backend-capf-mode-list 'clojurescript-mode)
-
-    (setq lsp-bridge-get-project-path-by-filepath
-          (lambda (filepath)
-            (or (when-let* ((project (project-current nil (file-name-directory filepath)))
-                            (root (project-root project)))
-                  (expand-file-name root))
-                (file-name-directory filepath))))
-
-    (yas-global-mode 1)
-    (global-lsp-bridge-mode)))
-
-(setup flymake
-  (:defer (:require flymake))
-  (:when-loaded
-    ;; ??:? `flymake-no-changes-timeout` ???? nil ?,
-    ;; ???? `eglot-handle-notification` ? `:after` ???
-    (:option flymake-no-changes-timeout nil
-             ;; emacs@30 feature
-             flymake-show-diagnostics-at-end-of-line 'fancy)
-    ;; flymake-fringe-indicator-position 'right-fringe)
-    (:with-mode prog-mode (:hook flymake-mode))
-    (:with-mode emacs-lisp-mode (:hook (lambda()(flymake-mode -1))))))
-
-(setup flymake-bridge
-  (:pkg (flymake-bridge :type git :host github :repo "eki3z/flymake-bridge"))
-  (add-hook 'lsp-bridge-mode-hook #'flymake-bridge-setup))
+;; (setup flymake-bridge
+;;   (:pkg (flymake-bridge :type git :host github :repo "eki3z/flymake-bridge"))
+;;   (add-hook 'lsp-bridge-mode-hook #'flymake-bridge-setup))
 
 
 ;; (setup flyover
@@ -209,37 +209,36 @@
 ;;   (:option flyover-show-at-eol t
 ;;            flyover-checkers '(flymake)))
 
-(when (featurep 'lsp-mode)
-  (setup corfu
-    (:defer (:pkg corfu))
-    (:when-loaded
-      (:with-feature nerd-icons-corfu
-        ;; Using VS Code icons as an alternative
-        (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
-      (global-corfu-mode)
-      (:option corfu-cycle t
-               corfu-auto t
-               corfu-auto-prefix 2
-               corfu-quit-no-match 'separator)
-      (:with-mode prog-mode (:hook corfu-mode))
-      (:with-mode corfu
-        (:bind "<escape>" corfu-quit
-               "<right>" corfu-quit
-               "TAB"  corfu-next
-               [tab]  corfu-next
-               "S-TAB"  corfu-previous
-               [backtab]  corfu-previous))
-      (:with-mode eshell-mode
-        (:local-set corfu-auto nil)
-        (corfu-mode))))
+(setup corfu
+  (:defer (:pkg corfu))
+  (:when-loaded
+    (:with-feature nerd-icons-corfu
+      ;; Using VS Code icons as an alternative
+      (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+    (global-corfu-mode)
+    (:option corfu-cycle t
+             corfu-auto t
+             corfu-auto-prefix 2
+             corfu-quit-no-match 'separator)
+    (:with-mode prog-mode (:hook corfu-mode))
+    (:with-mode corfu
+      (:bind "<escape>" corfu-quit
+             "<right>" corfu-quit
+             "TAB"  corfu-next
+             [tab]  corfu-next
+             "S-TAB"  corfu-previous
+             [backtab]  corfu-previous))
+    (:with-mode eshell-mode
+      (:local-set corfu-auto nil)
+      (corfu-mode))))
 
-  (setup cape
-    (:load-after corfu)
-    (:pkg cape)
-    (:when-loaded
-      (add-to-list 'completion-at-point-functions #'cape-emoji)
-      (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-      (add-to-list 'completion-at-point-functions #'cape-file))))
+(setup cape
+  (:load-after corfu)
+  (:pkg cape)
+  (:when-loaded
+    (add-to-list 'completion-at-point-functions #'cape-emoji)
+    (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+    (add-to-list 'completion-at-point-functions #'cape-file)))
 
 (provide 'init-completion)
 ;;; init-completion.el ends here
--- modules/init-util.el
+++ modules/init-util.el
@@ -261,5 +261,152 @@ See `consult-omni-multi' for more details.
         (consult-omni-brave-autosuggest input))))
   )
 
+(setup consult-omni
+  (:load-after consult)
+  (:option consult-omni-show-preview t ;;; show previews
+           consult-omni-preview-key "C-o"
+           consult-omni-highlight-matches-in-minibuffer t ;;; highlight matches in minibuffer
+           consult-omni-highlight-matches-in-file t ;;; highlight matches in files
+           consult-omni-default-count 5 ;;; set default count
+           consult-omni-default-page 0
+           consult-omni-dynamic-input-debounce 0.8
+           consult-omni-dynamic-input-throttle 1.6
+           consult-omni-dynamic-refresh-delay 0.8) ;;; set the preview key to C-o
+  (:when-loaded
+    ;; Load Sources Core code
+    (require 'consult-omni-sources)
+    ;; Load Embark Actions
+    (require 'consult-omni-embark)
+
+    ;; Either load all source modules or a selected list
+
+    ;; Select a list of modules you want to aload, otherwise all sources all laoded
+    (setq consult-omni-sources-modules-to-load (list
+                                                'consult-omni-apps
+                                                'consult-omni-brave-autosuggest
+                                                'consult-omni-brave
+                                                'consult-omni-browser-history
+                                                'consult-omni-buffer
+                                                'consult-omni-calc
+                                                'consult-omni-consult-notes
+                                                'consult-omni-dict
+                                                'consult-omni-fd
+                                                'consult-omni-find
+                                                'consult-omni-gh
+                                                'consult-omni-git-grep
+                                                'consult-omni-google
+                                                'consult-omni-google-autosuggest
+                                                ;; 'consult-omni-gptel
+                                                'consult-omni-grep
+                                                ;; 'consult-omni-invidious
+                                                'consult-omni-line-multi
+                                                'consult-omni-locate
+                                                'consult-omni-man
+                                                'consult-omni-mdfind
+                                                'consult-omni-org-agenda
+                                                'consult-omni-projects
+                                                'consult-omni-ripgrep
+                                                'consult-omni-ripgrep-all
+                                                'consult-omni-stackoverflow
+                                                'consult-omni-wikipedia
+                                                'consult-omni-youtube
+                                                ))
+
+    (consult-omni-sources-load-modules)
+
+    ;; set multiple sources for consult-omni-multi command. Change these lists as needed for different interactive commands. Keep in mind that each source has to be a key in `consult-omni-sources-alist'.
+    (setq consult-omni-multi-sources '("calc"
+                                       ;; "File"
+                                       ;; "Buffer"
+                                       ;; "Bookmark"
+                                       "Apps"
+                                       ;; "gptel"
+                                       ;; "Brave"
+                                       "Dictionary"
+                                       "Google"
+                                       "Wikipedia"
+                                       ;; "buffers text search"
+                                       "Notes Search"
+                                       "Org Agenda"
+                                       "GitHub"
+                                       "YouTube"))
+
+    ;; Per source customization
+
+    ;; Set API KEYs. It is recommended to use a function that returns the string for better security.
+    (setq consult-omni-google-customsearch-key "AIzaSyAcg1-rn_7FFO_QRMBbQgM0c2Tz_OgnzNI")
+    (setq consult-omni-google-customsearch-cx "068b46c41739e4141")
+    (setq consult-omni-brave-api-key "BSA3oU11HpInkmKaS3-Gk4_iy9l7Tyk")
+    (setq consult-omni-stackexchange-api-key "YOUR-STACKEXCHANGE-API-KEY-OR-FUNCTION")
+
+    ;; gptel settings
+    ;; (setq consult-omni-gptel-cand-title #'consult-omni--gptel-make-title-short-answer)
+
+    ;; default terminal
+    (setq consult-omni-embark-default-term #'vterm)
+
+    ;; default video player
+    ;; (setq consult-omni-embark-video-default-player  #'mpv-play-url)
+
+    ;; pretty prompt for launcher
+    (setq consult-omni-open-with-prompt "  ")
+
+    ;; Pick your favorite autosuggest command.
+    (setq consult-omni-default-autosuggest-command #'consult-omni-dynamic-google-autosuggest) ;;or any other autosuggest source you define
+
+    ;; Set your shorthand favorite interactive command
+    (setq consult-omni-default-interactive-command #'consult-omni-multi)
+
+    ;; Optionally Set back-end for notes search to ripgrep-all (requires ripgrep-all)
+    ;; (setq consult-omni-notes-backend-command "rga")
+
+    ;; Optionally add more interactive commands
+
+    ;; consult-omni-web
+    (defvar consult-omni-web-sources (list "Google"
+                                           "Wikipedia"
+                                           "GitHub"
+                                           "YouTube"
+                                           ))
+    (defun consult-omni-web (&optional initial prompt sources no-callback &rest args)
+      "Interactive web search”
+
+This is similar to `consult-omni-multi', but runs the search on
+web sources defined in `consult-omni-web-sources'.
+See `consult-omni-multi' for more details.
+"
+      (interactive "P")
+      (let ((prompt (or prompt (concat "[" (propertize "consult-omni-web" 'face 'consult-omni-prompt-face) "]" " Search:  ")))
+            (sources (or sources consult-omni-web-sources)))
+        (consult-omni-multi initial prompt sources no-callback args)))
+
+    ;; consult-omni-local
+    (defvar consult-omni-local-sources (list "ripgrep"
+                                             "mdfind"
+                                             "Notes Search"
+                                             "Apps"
+                                             "Org Agenda"))
+    (defun consult-omni-local (&optional initial prompt sources no-callback &rest args)
+      "Interactive local search”
+
+This is similar to `consult-omni-multi', but runs the search on
+local sources defined in `consult-omni-local-sources'.
+See `consult-omni-multi' for more details.
+"
+      (interactive "P")
+      (let ((prompt (or prompt (concat "[" (propertize "consult-omni-local" 'face 'consult-omni-prompt-face) "]" " Search:  ")))
+            (sources (or sources consult-omni-local-sources)))
+        (consult-omni-multi initial prompt sources no-callback args)))
+
+    ;; AutoSuggest at point
+    (defun consult-omni-autosuggest-at-point ()
+      (interactive)
+      (let ((input (or (thing-at-point 'url) (thing-at-point 'filename) (thing-at-point 'symbol) (thing-at-point 'sexp) (thing-at-point 'word))))
+        (when (and (minibuffer-window-active-p (selected-window))
+                   (equal (substring input 0 1) (consult--async-split-initial nil)))
+          (setq input (substring input 1)))
+        (consult-omni-brave-autosuggest input))))
+  )
+
 (provide 'init-util)
 ;;; init-util.el ends here

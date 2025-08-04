d4a9693aebc95839a697ebfb5df1ca694f95e7f2
dezzw <dw@dezzw.com>
2025-08-04 11:37:45 -0400

    Built by nix

--- flake.nix
+++ flake.nix
@@ -34,162 +34,60 @@
           inherit system;
           overlays = [ emacs-overlay.overlay ];
         };
-        mps = pkgs.callPackage ./mps.nix { };
       in
-      rec {
-        emacs-patched =
-          (pkgs.emacs-git.override {
-            withImageMagick = true;
-          }).overrideAttrs
-            (old: rec {
+        rec {
+          emacs-patched =
+            (pkgs.emacs-igc.override {
+              withImageMagick = true;
+            }).overrideAttrs
+              (old: rec {
+                patches = (old.patches or [ ]) ++ [
+                  # Fix OS window role so that yabai can pick up Emacs
+                  (pkgs.fetchpatch {
+                    url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+                    sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+                  })
 
-              name = "emacs-${version}";
-              version = "igc";
+                  # Add setting to enable rounded window with no decoration (still have to alter default-frame-alist)
+                  (pkgs.fetchpatch {
+                    url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-31/round-undecorated-frame.patch";
+                    sha256 = "sha256-WWLg7xUqSa656JnzyUJTfxqyYB/4MCAiiiZUjMOqjuY=";
+                  })
 
-              src = pkgs.fetchFromGitHub {
-                owner = "emacs-mirror";
-                repo = "emacs";
-                rev = "0a79b40c0e60ffb8d3591877a8d47d73d7aa4634";
-                hash = "sha256-8SV2c6zGlxhgWSIet9ZiudCSGFyQTNgDa07GH8yUclw=";
-              };
+                  # Make Emacs aware of OS-level light/dark mode
+                  (pkgs.fetchpatch {
+                    url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-31/system-appearance.patch";
+                    sha256 = "sha256-4+2U+4+2tpuaThNJfZOjy1JPnneGcsoge9r+WpgNDko=";
+                  })
 
-              configureFlags = (old.configureFlags or [ ]) ++ [
-                # "--with-xwidgets"
-                "--with-mps=yes"
-              ];
+                  ./patches/emacs-31/ns-alpha-background.patch
+                  ./patches/emacs-31/smooth-cursor.patch
+                ];
+              });
 
-              postPatch =
-                (old.postPatch or "")
-                + pkgs.lib.optionalString ((old ? NATIVE_FULL_AOT) || (old ? env.NATIVE_FULL_AOT)) (
-                  let
-                    backendPath = (
-                      pkgs.lib.concatStringsSep " " (
-                        builtins.map (x: ''\"-B${x}\"'') (
-                          [
-                            # Paths necessary so the JIT compiler finds its libraries:
-                            "${pkgs.lib.getLib pkgs.libgccjit}/lib"
-                            "${pkgs.lib.getLib pkgs.libgccjit}/lib/gcc"
-                            "${pkgs.lib.getLib pkgs.stdenv.cc.libc}/lib"
-                          ]
-                          ++ pkgs.lib.optionals (pkgs.stdenv.cc ? cc.libgcc) [
-                            "${pkgs.lib.getLib pkgs.stdenv.cc.cc.libgcc}/lib"
-                          ]
-                          ++ [
-                            # Executable paths necessary for compilation (ld, as):
-                            "${pkgs.lib.getBin pkgs.stdenv.cc.cc}/bin"
-                            "${pkgs.lib.getBin pkgs.stdenv.cc.bintools}/bin"
-                            "${pkgs.lib.getBin pkgs.stdenv.cc.bintools.bintools}/bin"
-                          ]
-                        )
-                      )
-                    );
-                  in
-                  ''
-                    substituteInPlace lisp/emacs-lisp/comp.el --replace-fail \
-                      "(defcustom comp-libgccjit-reproducer nil" \
-                      "(setq native-comp-driver-options '(${backendPath}))
-                       (defcustom comp-libgccjit-reproducer nil"
-                  '');
+          emacs-augmented = (
+            (pkgs.emacsPackagesFor emacs-patched).emacsWithPackages (
+              epkgs: with epkgs; [
+                # (callPackage ./site-packages/lsp-bridge/lsp-bridge.nix {
+                #   inherit (pkgs) fetchFromGitHub;
+                # })
 
-              # NIX_CFLAGS_COMPILE = "${old.NIX_CFLAGS_COMPILE or ""} -I${mps}/include";
-              # NIX_LDFLAGS = "${old.NIX_LDFLAGS or ""} -L${mps}/lib";
+                vterm
+                pdf-tools
+                pkgs.emacsPackages.treesit-grammars.with-all-grammars
+              ]
+            )
+          );
 
-              # preConfigure = ''
-              #   export CPPFLAGS="-I${mps}/include $CPPFLAGS}"
-              #   export LDFLAGS="-L${mps}/lib $LDFLAGS"
-              # '';
+          packages.demacs = emacs-augmented;
 
-              buildInputs =
-                old.buildInputs
-                ++ [ mps ]
-                ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (
-                  with pkgs.darwin.apple_sdk_12_3.frameworks;
-                  [
-                    Accelerate
-                    AppKit
-                    Carbon
-                    Cocoa
-                    GSS
-                    ImageCaptureCore
-                    ImageIO
-                    IOKit
-                    OSAKit
-                    Quartz
-                    QuartzCore
-                    WebKit
-                    UniformTypeIdentifiers
-                  ]
-                );
-
-              plistBuddy = "/usr/libexec/PlistBuddy";
-
-              postInstall =
-                old.postInstall
-                + ''
-                  app="$out/Applications/Emacs.app"
-                  plist="$app/Contents/Info.plist"
-                  path="/opt/homebrew/bin:/opt/homebrew/sbin:/Users/dez/.nix-profile/bin:/etc/profiles/per-user/dez/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:/Users/dez/.orbstack/bin:/Users/dez/Library/Application Support/JetBrains/Toolbox/scripts:/Users/dez/.local/bin:/Users/dez/.zsh/plugins/powerlevel10k:/Users/dez/.zsh/plugins/autopair:/Users/dez/.zsh/plugins/zsh-nix-shell"
-
-                  echo "Injecting PATH into $plist"
-
-                  # Use plistBuddy to modify the plist file
-                  ${plistBuddy} -c 'Add :LSEnvironment dict' "$plist" || true
-                  ${plistBuddy} -c "Add :LSEnvironment:PATH string '$path'" "$plist" || \
-                  ${plistBuddy} -c "Set :LSEnvironment:PATH '$path'" "$plist"
-
-                  # Update modification time
-                  touch "$app"
-                '';
-
-              patches = (old.patches or [ ]) ++ [
-                # Fix OS window role so that yabai can pick up Emacs
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
-                  sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
-                })
-
-                # Add setting to enable rounded window with no decoration (still have to alter default-frame-alist)
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
-                  sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
-                })
-
-                # Make Emacs aware of OS-level light/dark mode
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/system-appearance.patch";
-                  sha256 = "3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
-                })
-
-                ./patches/ns-alpha-background.patch
-                # ./patches/fix-xwidget.patch
-                # ./patches/cursor-animation.patch
-                # ./patches/blur.patch
-              ];
-            });
-
-        emacs-augmented = (
-          (pkgs.emacsPackagesFor emacs-patched).emacsWithPackages (
-            epkgs: with epkgs; [
-              (callPackage ./site-packages/lsp-bridge/lsp-bridge.nix {
-                inherit (pkgs) fetchFromGitHub;
-              })
-
-              vterm
-              pdf-tools
-              pkgs.emacsPackages.treesit-grammars.with-all-grammars
-            ]
-          )
-        );
-
-        packages.demacs = emacs-augmented;
-
-        apps.demacs = flake-utils.lib.mkApp {
-          drv = packages.demacs;
-          name = "demacs";
-          exePath = "/bin/emacs";
-        };
-        packages.default = packages.demacs;
-        apps.default = apps.demacs;
-      }
+          apps.demacs = flake-utils.lib.mkApp {
+            drv = packages.demacs;
+            name = "demacs";
+            exePath = "/bin/emacs";
+          };
+          packages.default = packages.demacs;
+          apps.default = apps.demacs;
+        }
     );
 }
